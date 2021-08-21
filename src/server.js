b1d013a7f6862a105f63fb16bd022e792a975366
이도현 <ksmfou98@naver.com>
2021-08-21 13:43:23 +0900

    Feat : Setting WebSocket Server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
@@ -7,7 +9,12 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/")); // /페이지를 제외한 다른 페이지에 들어가게 되면 /페이지로 리다이랙트
 
 const onListen = () => console.log(`Connect ✅ http://localhost:3000`);
 
-app.listen(3000, onListen);
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, onListen);
