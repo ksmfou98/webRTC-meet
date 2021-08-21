40ad7a1143a2fe92fbbf67ddfac58be8120bbafe
이도현 <ksmfou98@naver.com>
2021-08-21 10:38:15 +0900

    Frontend Setup

--- src/server.js
+++ src/server.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
+const onListen = () => console.log(`Connect ✅ http://localhost:3000`);
+
+app.listen(3000, onListen);
