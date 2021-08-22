3e226677368368a60004f5a8a50fc2449c02d0e3
이도현 <ksmfou98@naver.com>
2021-08-22 12:41:16 +0900

    Implements : WebRTC 1:1 화상

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# WebRTC-Meet
+# Noom
 
-Zoom Clone using NodeJS, WebRTC and Websockets
+Zoom Clone using NodeJS, WebRTC and Websockets.
--- src/server.js
+++ src/server.js
@@ -1,32 +1,34 @@
-import express from "express";
-import WebSocket from "ws";
 import http from "http";
+import SocketIO from "socket.io";
+import express from "express";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/")); // /페이지를 제외한 다른 페이지에 들어가게 되면 /페이지로 리다이랙트
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const onListen = () => console.log(`Connect ✅ http://localhost:3000`);
-
-const server = http.createServer(app);
-
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", (socket) => {
-  console.log("Connected to Browser ✅");
-
-  socket.addEventListener("close", () =>
-    console.log("Disconnected from Browser ❌")
-  );
-
-  socket.addEventListener("message", (message) => console.log(message.data));
-
-  socket.send("hello ! Dohyun");
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName) => {
+    console.log(roomName);
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
 });
 
-server.listen(3000, onListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
